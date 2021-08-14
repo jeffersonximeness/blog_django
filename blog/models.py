4d8a52c48e426890413351d5977386f2b8a80046
Jefferson <jeffersonximenes1@gmail.com>
2021-08-13 23:10:07 -0300

    Implementando os templates e criando logica de paginacao

--- blog/models.py
+++ blog/models.py
@@ -9,8 +9,7 @@ class PublishedManager(models.Manager):
 
 
     def get_queryset(self):
-        return super(PublishedManager,
-                     self).get_queryset().filter(status='published')
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -36,7 +35,7 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolut_url(self):
+    def get_absolute_url(self):
         return reverse('blog:post_detail',
                         args=[self.publish.year,
                               self.publish.month,
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
 
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post/list.html', {'page':page, 'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+
+    return render(request, 'blog/post/detail.html', {'post': post})
