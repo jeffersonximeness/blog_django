d92df2e74cb805a083700c6a02756e047e23da70
Jefferson <jeffersonximenes1@gmail.com>
2021-08-16 16:54:30 -0300

    Renderizando as tags no html

--- blog/views.py
+++ blog/views.py
@@ -44,8 +44,8 @@ def post_detail(request, year, month, day, post):
             new_comment = comment_form.save(commit=False)
             new_comment.post = post
             new_comment.save()
-        else:
-            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
 
     return render(request, 'blog/post/detail.html', {'post': post,
                                                      'comments': comments,
