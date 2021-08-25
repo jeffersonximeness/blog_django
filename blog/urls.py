a7776101aff1dd9cd48e10f152f0a767bb4c144b
Jefferson <jeffersonximenes1@gmail.com>
2021-08-24 21:40:10 -0300

    Implementando url da inscrição do feed RSS em base.html e adicionando padrao de url

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -10,4 +11,5 @@ urlpatterns = [
     #path('', views.PostListView.as_view(), name='post_list'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
